182a696ec4d8684f6da5ba67912aecb83b590d90
Basel Altishe <basel.altishe@gmail.com>
2020-03-04 23:32:01 +0100

    Made div elements for todos and todos-done objects

--- src/main.ts
+++ src/main.ts
@@ -3,60 +3,73 @@ import { Todo } from "./models/todo";
 window.onload = function() {
   let m = new Main();
   m.start();
-  m.newElement();
 };
 
 class Main {
   todos: Todo[] = [];
 
+  constructor() {
+    this.newElement = this.newElement.bind(this);
+  }
+
   generateHtml(todo: Todo) {
-    let divElement = document.createElement("div");
+    let allTodos = document.createElement("div");
+    let todoText = document.createElement("div");
+    allTodos.appendChild(todoText);
 
-    let descrContainer = document.createElement("div");
-    let descrText = document.createElement("span");
-
-    descrText.innerHTML =
+    todoText.innerHTML =
       todo.description + " " + todo.date.toLocaleTimeString();
 
-    todo.done ? (descrText.className = "done") : (descrText.className = "");
+    if (todo.done == true) {
+      todoText.className = "done";
+      let container = document.getElementById("doneDiv") as HTMLDivElement;
+      container.appendChild(allTodos);
+    } else {
+      todoText.className = "notdone";
+      let container = document.getElementById("todoDiv") as HTMLDivElement;
+      container.appendChild(allTodos);
+    }
 
-    descrContainer.appendChild(descrText);
-    divElement.appendChild(descrContainer);
-
-    let container = document.getElementById("container") as HTMLDivElement;
-    container.appendChild(divElement);
-
-    descrText.addEventListener("click", () => {
+    todoText.addEventListener("click", () => {
       todo.done = !todo.done;
       this.updatedTodoList();
     });
   }
 
   start() {
-    let firstTodo1 = new Todo(1, new Date(), "Test 1");
-    let firstTodo2 = new Todo(2, new Date(), "Test 2");
-    let firstTodo3 = new Todo(3, new Date(), "Test 3");
+    // let firstTodo1 = new Todo(1, new Date(), "Test 1");
+    // let firstTodo2 = new Todo(2, new Date(), "Test 2");
+    // let firstTodo3 = new Todo(3, new Date(), "Test 3");
 
-    this.todos.push(firstTodo1);
-    this.todos.push(firstTodo2);
-    this.todos.push(firstTodo3);
+    // this.todos.push(firstTodo1);
+    // this.todos.push(firstTodo2);
+    // this.todos.push(firstTodo3);
 
     this.todos.forEach(todo => {
       console.log(todo);
       this.generateHtml(todo);
     });
+
+    let input = document.getElementById("addBtn");
+    if (input !== null) {
+      input.addEventListener("click", this.newElement);
+    }
   }
 
   newElement() {
-    let inputValue = "";
-    inputValue = document.getElementById("myInput")?.nodeValue;
-    let t = document.createTextNode(inputValue);
+    let input = document.getElementById("myInput") as HTMLInputElement;
+    if (input !== null) {
+      let newValue = input.value;
+      let myTodo = new Todo(this.todos.length + 1, new Date(), newValue);
 
-    console.log(inputValue);
+      this.todos.push(myTodo);
+      this.updatedTodoList();
+    }
   }
 
   updatedTodoList() {
-    (document.getElementById("container") as HTMLDivElement).innerHTML = "";
+    (document.getElementById("todoDiv") as HTMLDivElement).innerHTML = "";
+    (document.getElementById("doneDiv") as HTMLDivElement).innerHTML = "";
     this.todos.forEach(todo => {
       console.log(todo);
       this.generateHtml(todo);
